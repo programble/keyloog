de9d6d9a41fb6ec0b54c450b74252a11d3e8f228
Curtis McEnroe <programble@gmail.com>
2011-04-11 21:03:20 -0400

    Made live updating statistics (to be moved to -f option later)

--- keycounter.c
+++ keycounter.c
@@ -16,37 +16,8 @@ void stop(int s)
     end = true;
 }
 
-int main(int argc, char** argv)
+void print_stats(int count, time_t start_time)
 {
-    signal(SIGTERM, stop);
-    signal(SIGQUIT, stop);
-    signal(SIGINT, stop);
-    
-    Display *display = XOpenDisplay(NULL);
-    if (!display) {
-        return -1;
-    }
-    
-    char keys_current[32], keys_last[32];
-    int count = 0;
-    time_t start_time = time(NULL);
-    
-    XQueryKeymap(display, keys_last);
-    while (!end) {
-        fflush(stdout);
-        usleep(5000);
-        XQueryKeymap(display, keys_current);
-        for (int i = 0; i < 32; i++) {
-            if (keys_current[i] != keys_last[i] && keys_current[i] != 0) {
-                count++;
-                //printf("\r%d", count);
-            }
-            keys_last[i] = keys_current[i];
-        }
-    }
-    XCloseDisplay(display);
-    
-    /* C is teh awsumz */
     long int elapsed_seconds = lrint(difftime(time(NULL), start_time));
     long int format_seconds = elapsed_seconds;
     long int format_minutes = format_seconds / 60;
@@ -70,6 +41,38 @@ int main(int argc, char** argv)
     printf("You pressed %.2f keys per minute\n", keys_per_second * 60);
     printf("You pressed %.2f keys per hour\n", keys_per_second * 60 * 60);
     printf("You pressed %.2f keys per day\n", keys_per_second * 60 * 60 * 24);
+}
+
+int main(int argc, char** argv)
+{
+    signal(SIGTERM, stop);
+    signal(SIGQUIT, stop);
+    signal(SIGINT, stop);
     
+    Display *display = XOpenDisplay(NULL);
+    if (!display) {
+        return -1;
+    }
+    
+    char keys_current[32], keys_last[32];
+    int count = 0;
+    time_t start_time = time(NULL);
+    
+    XQueryKeymap(display, keys_last);
+    while (!end) {
+        fflush(stdout);
+        usleep(5000);
+        XQueryKeymap(display, keys_current);
+        for (int i = 0; i < 32; i++) {
+            if (keys_current[i] != keys_last[i] && keys_current[i] != 0) {
+                count++;
+                /* TODO: Move this next part to a -f option */
+                printf("\x1b[2J\x1b[H"); /* Clear and home */
+                print_stats(count, start_time);
+            }
+            keys_last[i] = keys_current[i];
+        }
+    }
+    XCloseDisplay(display);
     return 0;
 }

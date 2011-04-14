e340bc6ce3a8dbfd091e108d61e794e0b8e2ae00
Curtis McEnroe <programble@gmail.com>
2011-04-14 16:42:47 -0400

    Moved things around a bunch

--- keycounter.c
+++ keycounter.c
@@ -1,8 +1,8 @@
-/* For usleep() */
-#define _BSD_SOURCE
+#define _BSD_SOURCE // For usleep() and getdtablesize()
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
+
 #include <errno.h>
 #include <fcntl.h>
 #include <getopt.h>
@@ -17,10 +17,42 @@
 
 #define PLURAL(x) (x != 1) ? "s" : ""
 
-int count;
-time_t start_time;
-char *option_file = NULL;
+void output_stats();
 
+void daemonize(const char *option_pidfile)
+{
+    pid_t pid = fork();
+    if (pid < 0) {
+        perror("fork");
+        exit(1);
+    } else if (pid > 0) {
+        // Write child PID to file
+        if (option_pidfile) {
+            FILE *pidfile = fopen(option_pidfile, "w");
+            if (!pidfile) {
+                perror(option_pidfile);
+                exit(1);
+            }
+            fprintf(pidfile, "%d\n", pid);
+            fclose(pidfile);
+        }
+        exit(0);
+    }
+    
+    // Detach from controlling terminal
+    setsid();
+    
+    // Close all file descriptors
+    for (int i = getdtablesize(); i >= 0; i--)
+        close(i);
+    
+    // Open stdin, stdout, stderr to /dev/null
+    int fd = open("/dev/null", O_RDWR);
+    dup(fd);
+    dup(fd);
+}
+
+// Signal handlers
 bool end = false;
 
 void signal_quit(int s)
@@ -28,7 +60,16 @@ void signal_quit(int s)
     end = true;
 }
 
-void print_stats()
+void signal_hup(int s)
+{
+    output_stats();
+}
+
+int count = 0;
+time_t start_time;
+char *option_file = NULL;
+
+void output_stats()
 {
     FILE *file = stdout;
     if (option_file) {
@@ -71,60 +112,18 @@ void print_stats()
         fclose(file);
 }
 
-void signal_hup(int s)
-{
-    print_stats();
-}
-
-void daemonize(const char *option_pidfile)
-{
-    // Fork
-    pid_t pid = fork();
-    if (pid < 0) {
-        fprintf(stderr, "Error: Could not fork\n");
-        exit(1);
-    } else if (pid > 0)
-        exit(0); // Exit parent
-    
-    // Write out PID file
-    if (option_pidfile) {
-        pid_t pid = getpid();
-        FILE *pidfile = fopen(option_pidfile, "w");
-        if (!pidfile) {
-            perror(option_pidfile);
-            exit(1);
-        }
-        fprintf(pidfile, "%d\n", pid);
-        fclose(pidfile);
-    }
-    
-    // Detach from controlling terminal
-    setsid();
-    
-    // Close all file descriptors
-    for (int i = getdtablesize(); i >= 0; i--)
-        close(i);
-    
-    // Open stdin, stdout, stderr to /dev/null
-    int fd = open("/dev/null", O_RDWR);
-    dup(fd);
-    dup(fd);
-}
-
 void print_usage(const char *exec_name)
 {
     printf("Usage: %s [OPTION]... [FILE]\n\n", exec_name);
     printf("  -f, --follow          output keypress statistics as they update\n");
     printf("  -d, --daemonize       run in the background\n");
-    printf("  -p, --pid-file=FILE   file to write PID to\n");
+    printf("  -p, --pid-file=FILE   write PID to FILE\n");
     printf("  -h, --help            display this help and exit\n");
 }
 
-int main(int argc, char** argv)
+int main(int argc, char *argv[])
 {
-    bool option_follow = false, option_daemonize = false;
-    char *option_pidfile = NULL;
-    
+    // Parse command-line options
     static struct option long_options[] = {
         {"follow", no_argument, NULL, 'f'},
         {"daemonize", no_argument, NULL, 'd'},
@@ -133,9 +132,12 @@ int main(int argc, char** argv)
         {0, 0, 0, 0}
     };
     
-    char c;
-    while ((c = getopt_long(argc, argv, "fdp:h", long_options, NULL)) != -1) {
-        switch(c) {
+    bool option_follow = false, option_daemonize = false;
+    char *option_pidfile = NULL;
+    
+    char o;
+    while ((o = getopt_long(argc, argv, "fdp:h", long_options, NULL)) != -1) {
+        switch (o) {
         case 'f':
             option_follow = true;
             break;
@@ -148,22 +150,15 @@ int main(int argc, char** argv)
         case 'h':
             print_usage(argv[0]);
             return 0;
-        case '?':
+        case '?': // Unrecognized argument
             return 1;
         }
-    }
-    
-    if (option_follow && option_daemonize) {
-        fprintf(stderr, "Error: Cannot follow and daemonize\n");
-        return 1;
     }
     
     if (optind < argc)
         option_file = argv[optind];
     
-    if (option_daemonize)
-        daemonize(option_pidfile);
-    
+    // Set up signals
     signal(SIGTERM, signal_quit);
     signal(SIGQUIT, signal_quit);
     signal(SIGINT, signal_quit);
@@ -171,43 +166,45 @@ int main(int argc, char** argv)
     signal(SIGHUP, signal_hup);
     signal(SIGUSR1, signal_hup);
     
-    // FIXME: Perhaps move this to before daemonization, so the error is
-    // visible
+    // Daemonize
+    if (option_daemonize)
+        daemonize(option_pidfile);
+    
+    // Open X display
     Display *display = XOpenDisplay(NULL);
     if (!display) {
         fprintf(stderr, "Error: Could not open X display\n");
         return 1;
     }
     
-    char keys_current[32], keys_last[32];
-    count = 0;
-    start_time = time(NULL);
-    
-    if (option_follow) {
+    if (option_follow)
         printf(TERM_CLEAR TERM_HOME);
-        print_stats();
-    }
+    
+    start_time = time(NULL);
+    char keys_current[32], keys_last[32];
     
     XQueryKeymap(display, keys_last);
     while (!end) {
         fflush(stdout);
         usleep(5000);
+        
         XQueryKeymap(display, keys_current);
         for (int i = 0; i < 32; i++) {
-            if (keys_current[i] != keys_last[i] && keys_current[i] != 0) {
+            if (keys_current[i] > keys_last[i]) {
                 count++;
                 if (option_follow) {
                     printf(TERM_CLEAR TERM_HOME);
-                    print_stats();
+                    output_stats();
                 }
             }
             keys_last[i] = keys_current[i];
         }
     }
+    
     XCloseDisplay(display);
     
     if (!option_follow)
-        print_stats();
+        output_stats();
     
     return 0;
 }
